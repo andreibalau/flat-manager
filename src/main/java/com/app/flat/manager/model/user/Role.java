28660fdf053375ca63e168ee2f94e538cccea6e5
Catalin M <catalin.matache@fortech.ro>
2020-02-17 17:53:23 +0200

    update the role

--- src/main/java/com/app/flat/manager/model/user/Role.java
+++ src/main/java/com/app/flat/manager/model/user/Role.java
@@ -5,7 +5,7 @@ package com.app.flat.manager.model.user;
  * Created by catalin on 1/20/2020
  */
 public enum Role {
-	ROLE_PROPRIETARY,
-	ROLE_STAIR_PRESIDENT,
-	ROLE_ADMINISTRATOR
+	PROPRIETARY,
+	ADMINISTRATOR,
+	PRESIDENT
 }
